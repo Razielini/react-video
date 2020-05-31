301a86e7b92712c000b7c81422e150fb3644e85b
Razielini <razielco@gmail.com>
2020-05-30 21:29:39 -0600

    Create Contact.js

---
+++ src/pages/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './generic-page.css';
+
+const Contact = () => (
+  <div className="Page">
+    <h1>Contacto</h1>
+    <p>Esto es una pagina de contacto</p>
+  </div>
+)
+
+export default Contact
