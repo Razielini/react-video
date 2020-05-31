78e0f9b94d0f57b9d41d09c0fd930789e535f948
Razielini <razielco@gmail.com>
2020-05-30 21:29:54 -0600

    Create NotFound.js

---
+++ src/pages/components/NotFound.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from "react";
+
+class NotFound extends PureComponent {
+  render () {
+    return (
+      <div>
+        <h1>404</h1>
+        <h3>:(</h3>
+        <h2>No hemos encontrado lo que buscabas.</h2>
+      </div>
+    )
+  }
+}
+
+export default NotFound
