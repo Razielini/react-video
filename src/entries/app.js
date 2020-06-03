689cbf869513f4d343a41d1766b0f11983f1a7e9
Razielini <razielco@gmail.com>
2020-06-02 19:35:03 -0600

    Update app.js

--- src/entries/app.js
+++ src/entries/app.js
@@ -1,83 +1,13 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-import Videos from '../pages/containers/Videos';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reducer from '../reducers/index';
-import { Map as map } from 'immutable';
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
-
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Header from '../pages/components/Header'
-import Home from '../pages/components/Home'
-import Contact from '../pages/components/Contact';
-import NotFound from '../pages/components/NotFound';
-// function logger({ getState, dispatch}) {
-//   return (next) => {
-//     return (action) => {
-//       console.log('este es mi viejo estado', getState().toJS())
-//       console.log('vamos a enviar está acción', action);
-//       const value = next(action)
-//       console.log('este es mi nuevo estado', getState().toJS())
-//       return value
-//     }
-//   }
-// }
-
-
-const logger_ = ({getState, dispatch }) => next => action => {
-  console.log('este es mi viejo estado', getState().toJS())
-  console.log('vamos a enviar está acción', action);
-  const value = next(action)
-  console.log('este es mi nuevo estado', getState().toJS())
-  return value
-}
-
-const store = createStore(
-  reducer,
-  map(),
-  composeWithDevTools(
-    applyMiddleware(
-      logger,
-      thunk
-    )
-  )
-);
-
+import App from '../pages/containers/app'
+import { BrowserRouter } from 'react-router-dom';
 
 const homeContainer = document.getElementById('home-container')
 
-
 render(
   <BrowserRouter>
-    <Provider store={store}>
-      <Fragment>
-        <Header />
-        <Switch>
-          <Route
-            exact
-            path="/"
-            component={Home}
-          />
-          <Route
-            exact
-            path="/videos"
-            component={Videos}
-          />
-          <Route
-            exact
-            path="/contacto"
-            component={Contact}
-          />
-          <Route
-            path="*"
-            component={NotFound}
-          />
-        </Switch>
-      </Fragment>
-    </Provider>
+    <App />
   </BrowserRouter>
 , homeContainer);
 
